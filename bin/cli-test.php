42fd02450cf45a80026aa64ba1ebf1951e3a9b90
rlf <rlf@users-MacBook-Pro.local>
2018-06-05 14:21:08 +0200

    test

--- bin/cli-test.php
+++ bin/cli-test.php
@@ -1,17 +1,23 @@
 <?php
 
-include(__DIR__ . "/../pimcore/config/startup_cli.php");
+use Pimcore5\DeploymentBundle\Config\Config;
 
-use Pimcore\Model\DataObject;
+require_once __DIR__ . '/bootstrap.php';
 
-// create some random objects ;-)
-for ($i = 0; $i < 60; $i++) {
-//    $o = new DataObject\News();
-//    $o->setKey(uniqid() . "-" . $i);
-//    $o->setParentId(1);
-//    $o->setPublished(true);
-//    $o->save();
-    echo("Have not created object " . $i . "\n");
-}
+//use Pimcore\Model\DataObject;
+
+//// create some random objects ;-)
+//for ($i = 0; $i < 60; $i++) {
+////    $o = new DataObject\News();
+////    $o->setKey(uniqid() . "-" . $i);
+////    $o->setParentId(1);
+////    $o->setPublished(true);
+////    $o->save();
+//    echo("Have not created object " . $i . "\n");
+//}
+//
+
+
+echo ' == Config test: == ' . Config::get('url') . ' == ';
 
 
