2d1721181246e7e08a889aeea79149139c656dfb
Roelf <geneticallyalteredmastermind@gmail.com>
2018-07-30 14:06:30 +0200

    Update bootstrap.php

--- bin/bootstrap.php
+++ bin/bootstrap.php
@@ -1,2 +1,19 @@
 <?php
-include(__DIR__ . "/../../../../pimcore/config/startup_cli.php");   // new pimcore
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+require (__DIR__ . '/../../../../pimcore/lib/Pimcore/Bootstrap.php');
+
+$kernel = \Pimcore\Bootstrap::startupCli();
+
+return $kernel;
